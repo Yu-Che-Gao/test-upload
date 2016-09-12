579b0eae36a279ea15ebd882345c5c9a22eb5789
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-09-13 00:15:24 +0800

    測試版本
    Signed-off-by: Yu-Che-Gao <s0258037@gm.csmu.edu.tw>

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+    $im = imagecreatetruecolor(120, 20);
+    $text_color = imagecolorallocate($im, 233, 14, 91);
+    imagestring($im, 1, 5, 5,  'A Simple Text String', $text_color);
+    imagejpeg($im, 'upload.jpg', 100);
+    imagedestroy($im);
+?>
